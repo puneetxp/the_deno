a1125fb58d3c3cabdf1f7a5765cc83cb2da98170
Puneet Sharma <puneetsharma9@hotmail.com>
2023-08-18 19:45:32 +0530

    update depts

--- deps.ts
+++ deps.ts
@@ -2,4 +2,4 @@ export { crypto } from "https://deno.land/std@0.198.0/crypto/mod.ts";
 export { encode } from "https://deno.land/std@0.198.0/encoding/base64.ts";
 export { getSetCookies, getCookies, setCookie, deleteCookie } from "https://deno.land/std@0.198.0/http/cookie.ts";
 export type { Cookie } from "https://deno.land/std@0.198.0/http/cookie.ts";
-export * as mysql2 from "npm:mysql2";
+export * as mysql2 from "npm:mysql2@3.6.0";
